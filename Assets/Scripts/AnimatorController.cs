0b47b48de7e6bd93211e118ea97c233b2a1d8c97
Jacob <goldenneedham@gmail.com>
2019-07-27 13:17:49 -0700

    added some sprites and animation controller

---
+++ Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public Animator anim;
+
+
+    public void Start(){
+        rigid = this.GetComponent<Rigidbody2D>();
+        if(anim == null){
+            anim = this.GetComponent<Animator>();
+        }
+    }
+
+    public void Update(){
+        anim.SetFloat("Horizontal", rigid.velocity.x);
+        anim.SetFloat("Vertical", rigid.velocity.y);
+    }
+}
