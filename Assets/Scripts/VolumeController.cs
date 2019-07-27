18c8ef8f011c1893897edc7240dc2e365f5efb46
sbarrack <12619603+sbarrack@users.noreply.github.com>
2019-07-27 13:17:07 -0700

    volume controller

---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class VolumeController : MonoBehaviour
+{
+    private Slider slider;
+    public TMP_Text value;
+    private const string key = "volume";
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        float savedVolume = PlayerPrefs.GetFloat(key, 1f);
+        slider.value = savedVolume;
+        changeVolume();
+    }
+
+    public void changeVolume()
+    {
+        value.text = Mathf.Floor(slider.value * 100f).ToString() + "%";
+        PlayerPrefs.SetFloat(key, slider.value);
+    }
+}
